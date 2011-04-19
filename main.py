3c4f56998bf3499cc9f623b64bea366bef3c69ce
Rob Fitzpatrick <robftz@gmail.com>
2011-04-19 15:09:58 +0100

    Initial commit. Most page skeletons and partially complete dashboard.

---
+++ main.py
@@ -0,0 +1,172 @@
+import os
+import cgi
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+import appengine_admin
+import models
+
+
+class MainPage(webapp.RequestHandler):
+
+    def get(self):
+
+        user = users.get_current_user()
+
+        if user:
+
+            acct_url = users.create_logout_url(self.request.uri)
+            acct_url_linktext = 'Logout'
+
+            header_c2a_url = "/dashboard"
+            header_c2a_linktext = "Dashboard"
+
+        else:
+            acct_url = users.create_login_url(self.request.uri)
+            acct_url_linktext = 'Login'
+
+            header_c2a_url = "/new"
+            header_c2a_linktext = "Get started"
+
+        template_values = {
+                'header_c2a_url': header_c2a_url,
+                'header_c2a_linktext': header_c2a_linktext,
+                'acct_url': acct_url,
+                'acct_url_linktext': acct_url_linktext
+            }
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+class NewProject(webapp.RequestHandler):
+
+    def get(self):
+
+        user = users.get_current_user()
+        if user:
+
+            project = None
+
+            if not project:
+
+                default_flow = models.FlowTemplate.all().filter("is_default =", True)[0]
+
+                project = models.Company(
+                        owner=user,
+                        name="A wonderful company",
+                        flow_template=default_flow)
+
+                project.put()
+
+            self.redirect('/dashboard')
+
+        else:
+            self.redirect(users.create_login_url('/new'))
+
+class SaveHypothesis_ajax(webapp.RequestHandler):
+
+    def post(self):
+
+        user = users.get_current_user()
+        company = models.Company.all().filter("owner =", user)[0]
+        progress = models.Progress.all().filter("company =", company).order("-order")
+
+        key = self.request.get("key")
+        hypothesis = self.request.get("hypothesis")
+
+        current_progress = models.Progress.get(key)
+        current_progress.hypothesis = hypothesis
+        current_progress.put()
+
+
+class SaveEvidence(webapp.RequestHandler):
+
+    def post(self):
+
+        user = users.get_current_user()
+        company = models.Company.all().filter("owner =", user)[0]
+        progress = models.Progress.all().filter("company =", company).order("-order")
+
+        key = self.request.get("key")
+        evidence = self.request.get("evidence")
+
+        current_progress = models.Progress.get(key)
+        current_progress.evidence = evidence
+        current_progress.put()
+
+        next_step = current_progress.step.next()
+        next_progress = models.Progress(
+                step=next_step,
+                company=company,
+                order=models.Progress.all().filter("company =", company).count())
+        next_progress.put()
+
+        self.redirect('/dashboard')
+
+
+class Dashboard(webapp.RequestHandler):
+
+
+
+
+    def get(self):
+
+        user = users.get_current_user()
+
+        if not user:
+
+            self.redirect(users.create_login_url('/dashboard'))
+            return
+
+        company = models.Company.all().filter("owner =", user)[0]
+
+        progress = models.Progress.all().filter("company =", company).order("-order")
+        flow = company.flow_template
+        steps = models.StepTemplate.all().filter("flow =", flow).order("order")
+
+        if progress.count() == 0:
+            first_progress = models.Progress(step=steps[0],
+                    company=company,
+                    order=0)
+            first_progress.put()
+
+        current_progress = progress[0]
+        progress = progress[1:progress.count()]
+
+        template_values = {
+                'header_c2a_url': '/dashboard/',
+                'header_c2a_linktext': 'Dashboard',
+                'acct_url': users.create_logout_url(self.request.uri),
+                'acct_url_linktext': 'Logout',
+                'steps': steps,
+                'progress': progress,
+                'current_progress': current_progress,
+
+            }
+        path = os.path.join(os.path.dirname(__file__), 'dashboard.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+application = webapp.WSGIApplication([
+
+        ('/', MainPage),
+        ('/new/', NewProject),
+        ('/new', NewProject),
+        ('/dashboard/hypothesis/', SaveHypothesis_ajax),
+        ('/dashboard/evidence/', SaveEvidence),
+        ('/dashboard', Dashboard),
+        ('/dashboard/', Dashboard),
+        (r'^(/admin)(.*)$', appengine_admin.Admin),
+
+        ], 
+        debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,66 @@
+from google.appengine.ext import db
+import appengine_admin
+
+
+
+class FlowTemplate(db.Model):
+
+    name = db.StringProperty()
+
+    is_default = db.BooleanProperty(default=False)
+
+
+class StepTemplate(db.Model):
+
+    name = db.StringProperty()
+    order = db.IntegerProperty()
+
+    flow = db.ReferenceProperty(FlowTemplate)
+    icon_url_base = db.StringProperty()
+
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def icon_big(self):
+        return "/media/%s_64.jpg" % self.icon_url_base
+
+    def icon_small(self):
+        return "%s_32.jpg" % self.icon_url_base
+
+    def next(self):
+        try:
+            return StepTemplate.all().filter("flow =", self.flow).filter("order =", self.order + 1)[0]
+        except:
+            return self
+
+
+class Company(db.Model):
+
+    owner = db.UserProperty()
+    name = db.StringProperty()
+
+    flow_template = db.ReferenceProperty(FlowTemplate)
+
+
+class Progress(db.Model):
+
+    step = db.ReferenceProperty(StepTemplate)
+    company = db.ReferenceProperty(Company)
+
+    order = db.IntegerProperty()
+
+    hypothesis = db.StringProperty(default="", multiline=True)
+    evidence = db.StringProperty(default="", multiline=True)
+
+
+class AdminFlowTemplate(appengine_admin.ModelAdmin):
+    model = FlowTemplate
+
+
+class AdminStepTemplate(appengine_admin.ModelAdmin):
+    model = StepTemplate
+
+appengine_admin.register(AdminFlowTemplate, AdminStepTemplate)
+
+
